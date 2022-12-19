b4e0558ea295b0aada41620bebccc25da45ce6ff
Jasmine Molano <jasmine.molanocorreia@heig-vd.ch>
2022-12-19 10:09:36 +0100

    added environment files

---
+++ environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    // That's the same api Url in our case, but in real project, it would certainly be different (you don't want to develop using the same instance as the production application...)
+    apiUrl: "https://zenith.onrender.com",
+  };
