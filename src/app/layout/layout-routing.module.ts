f52ce43feae4f05b08c7fed597a0782c7a20b93e
Jasmine Molano <jasmine.molanocorreia@heig-vd.ch>
2022-12-19 09:12:53 +0100

    Redirect to home

--- src/app/layout/layout-routing.module.ts
+++ src/app/layout/layout-routing.module.ts
@@ -8,10 +8,17 @@ const routes: Routes = [
     component: LayoutPage,
     children: [
       {
+        path: "",
+        redirectTo: "home", // Or whatever tabs is your default one
+        pathMatch: "full",
+      },
+      {
+        // Route that loads the home module
         path: 'home',
         loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
       },
       {
+        // Route that loads the account module
         path: 'account',
         loadChildren: () => import('./account/account.module').then(m => m.AccountPageModule)
       }
