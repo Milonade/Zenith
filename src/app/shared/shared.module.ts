d13f9b95be732904ca257ec1dce064f429302229
Jasmine Molano <jasmine.molanocorreia@heig-vd.ch>
2022-12-19 15:40:46 +0100

    ??

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
-import { SearchBarComponent } from './components/search-bar/search-bar.component';
-import { HeaderComponent } from './components/header/header.component';
+import { HeaderComponent } from '../components/header/header.component';
+import { SearchBarComponent } from '../components/search-bar/search-bar.component';
+
 
 
 
