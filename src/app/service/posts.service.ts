6fbe6458a03b2dfc02133c8def2753408f4e6a6e
Jasmine Molano <jasmine.molanocorreia@heig-vd.ch>
2023-01-16 16:42:00 +0100

    working on qimg API

--- src/app/service/posts.service.ts
+++ src/app/service/posts.service.ts
@@ -2,16 +2,20 @@ import { Injectable, OnInit } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 
 import { Observable} from "rxjs";
-
-
+import { environment } from "src/environments/environment";
 import { Post } from '../models/post';
 
 @Injectable({ providedIn: "root" })
 
 export class PostService  {
     constructor(private http: HttpClient) { }
+
     getPosts(): Observable<Post[]> {
-        return this.http.get<Post[]>('https://zenith.onrender.com/posts');
+        return this.http.get<Post[]>(`${environment.apiUrl}/posts`);
+    }
+    
+    uploadImage(): Observable<any> {
+        return this.http.post<any>(`${environment.qimgUrl}`);
     }
    
 }
