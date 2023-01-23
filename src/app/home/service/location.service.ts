1453f0237924bde13c22aa107d325cd4362a4429
Meryl Dubois <meryl.dubois@heig-vd.ch>
2023-01-23 12:52:58 +0100

    comment getCurrentPos

--- src/app/home/service/location.service.ts
+++ src/app/home/service/location.service.ts
@@ -2,12 +2,24 @@ import { Geolocation } from '@capacitor/geolocation';
 import { Injectable, OnInit } from "@angular/core";
 import { Location } from 'src/app/models/location';
 import { Observable } from "rxjs";
+import { latLng } from 'leaflet';
 
 
 @Injectable({ providedIn: "root" })
 
 export class locationService {
     // Geolocation
+
+    // C'est pas super utile vu que ça fait la même chose
+    // async getCurrentPos(): Promise<Location> {
+    //     const currentPos = await Geolocation.getCurrentPosition()
+
+    //     return {
+    //         lat: currentPos.coords.latitude,
+    //         lng: currentPos.coords.longitude
+    //     }
+    // }
+
     // addPosition$(location: Location): Observable<void> {
 
     // }
